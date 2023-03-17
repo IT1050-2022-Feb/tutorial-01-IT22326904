416378fade674fc1f90b09560078f745df39ecb3
Asiri Alwis <asirichathurika900@gmail.com>
2023-03-17 22:18:53 +0530

    Completed Ex_01

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,16 @@
 #include <stdio.h>
 
 int main() {
-  
+  float mark1 , mark2 , ave;
+  printf ("Enter mark1 :");
+  scanf ("%f" , &mark1);
+
+  printf ("Enter mark1 ");
+  scanf ("%f" , &mark2);
+
+  ave = (mark1 + mark2)/2;
+
+  printf ("Average is %.2f", ave);
   return 0;
 }
 
